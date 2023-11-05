db1b6a9552302f61c2adb84c56a4d82e94a03acc
Hopearceus <jxc2928@buaa.edu.cn>
2023-11-05 22:21:34 +0800

    [fix]set Mars max run step

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def run():
     os.system((pathlib.Path(xilinx_path)/"bin"/"nt64"/"fuse").__str__() +
               " -nodebug -prj ./run/mips.prj -o mips.exe mips_tb")
     s = input("Do you want to enable delay branch? [y/N]")
-    no_db = s.lower().find("y") != -1
+    no_db = s.lower().find("y") == -1
     for _ in gen_data(10):
         os.system(java_path + " -jar ./Mars_CO_v0.4.1.jar mc CompactLargeText a dump .text HexText ./temporary/code.txt ./temporary/test.asm")
         os.system(r"move .\temporary\code.txt .\code.txt")
@@ -26,10 +26,10 @@ def run():
 
         if no_db:
             os.system(java_path +
-                      " -jar Mars_CO_v0.4.1.jar ig mc CompactLargeText nc ./temporary/test.asm coL1 > ./temporary/ans.txt")
+                      " -jar Mars_CO_v0.4.1.jar ig 1000000 mc CompactLargeText nc ./temporary/test.asm coL1 > ./temporary/ans.txt")
         else:
             os.system(java_path +
-                      " -jar Mars_CO_v0.4.1.jar ig db mc CompactLargeText nc ./temporary/test.asm coL1 > ./temporary/ans.txt")
+                      " -jar Mars_CO_v0.4.1.jar ig 1000000 db mc CompactLargeText nc ./temporary/test.asm coL1 > ./temporary/ans.txt")
 
         if not diff(pathlib.Path("./temporary/ans.txt").read_text(), pathlib.Path("./temporary/cpu.txt").read_text()):
             break
