e8c03f77f67f16f3de701580dfdf3613cbd47b7a
kai_Ker <kai_Ker@buaa.edu.cn>
2023-11-07 00:09:05 +0800

    [feat] Add colorful prompt when asking the config

--- SetPath.py
+++ SetPath.py
@@ -21,26 +21,29 @@ def set_path():
 
 
 def write_config():
+    def hint_wrapper(prompt: str, severity=False):
+        return ('\033[1m\033[31m' if severity else '\033[1m\033[36m') + prompt + '\033[0m'
+
     if not os.path.exists("./config"):
         os.mkdir("config")
-    s = input(f"Please enter your ISE path (end with ISE{os.path.sep}): ")
+    s = input(hint_wrapper(f"Please enter your ISE path (end with ISE{os.path.sep}): "))
     while re.match(".*[/\\\\]ISE[/\\\\]?$", s) is None or not pathlib.Path(s).exists() or not pathlib.Path(s).is_dir():
-        s = input("Error path! Try again: ")
+        s = input(hint_wrapper("Error path! Try again: ", True))
     pathlib.Path("config/ISE_path").write_text(s)
 
-    s = input("Please enter your Java path: ")
+    s = input(hint_wrapper("Please enter your Java path: "))
     right = False
     while not right:
         try:
             subprocess.check_call([s, "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             right = True
         except (FileNotFoundError, subprocess.CalledProcessError) as _:
-            s = input("Error path! Try again: ")
+            s = input(hint_wrapper("Error path! Try again: ", True))
 
     pathlib.Path("config/Java_path").write_text(s)
-    s = input("Please enter your ise project path: ")
+    s = input(hint_wrapper("Please enter your ise project path: "))
     while not pathlib.Path(s).exists():
-        s = input("No such folder! Try again: ")
+        s = input(hint_wrapper("No such folder! Try again: ", True))
     pathlib.Path("config/Prj_path").write_text(s)
 
 
