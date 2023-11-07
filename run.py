0c3b8efa8955adc21f91ba78cbc4993e2e3b3920
kai_Ker <kai_Ker@buaa.edu.cn>
2023-11-07 11:06:42 +0800

    [feat] Move the running path into the folder `run`

--- run.py
+++ run.py
@@ -8,8 +8,12 @@ from JudgeCore import diff
 from DataSpawner import gen_data
 
 
+current_path = pathlib.Path(__file__).resolve().parent
+
+
 def hint_wrapper(s):
     return '\033[1m\033[34m' + s + '\033[0m'
+
 
 def prompt_wrapper(s):
     return '\033[1m\033[36m' + s + '\033[0m'
@@ -19,29 +23,36 @@ def run():
     path = sp.set_path()
     xilinx_path = path['xilinx_path']
     java_path = path['java_path']
+
     # prj_path = path['prj_path']
     # cwd_path = os.getcwd()
     # for file in pathlib.Path(prj_path).rglob("*\\*.v"):
     #     print("copy " + str(file) + " " + cwd_path + "\\run\\" + file.name)
     #     os.system("copy " + str(file) + " " + cwd_path + "\\run\\" + file.name)
     # os.system("copy " + prj_path + "\\mips.prj " + cwd_path + "run\\mips.prj")
+
     print(hint_wrapper("Compiling the verilog projects ..."))
-    if platform.system() == "Windows":
-        os.system((pathlib.Path(xilinx_path) / "bin" / "nt64" / "fuse").__str__() +
-                  " -nodebug -prj ./run/mips.prj -o mips.exe mips_tb")
-    else:
-        os.system((pathlib.Path(xilinx_path) / "bin" / "lin64" / "fuse").__str__() +
-                  " -nodebug -prj ./run/mips.prj -o mips.exe mips_tb")
+    bin_folder = "nt64" if platform.system() == "Windows" else "lin64"
+    fuse_path = (pathlib.Path(xilinx_path) / 'bin' / bin_folder / 'fuse')
+    os.chdir(current_path / "run")
+    ret = os.system(f"{fuse_path} -nodebug -prj mips.prj -o mips.exe mips_tb")
+    os.chdir(current_path)
+    print(ret)
     print(hint_wrapper("Done!"))
+
     s = input(prompt_wrapper("Enable the delay branch? [y/N] "))
     no_db = s.lower().find("y") == -1
+
     for _ in gen_data(10):
         print(hint_wrapper("Run Mars to get the code text ..."))
         os.system(java_path + " -jar ./Mars_CO_v0.4.1.jar nc mc CompactLargeText a dump .text HexText ./temporary/code.txt ./temporary/test.asm")
         # os.system(r"move .\temporary\code.txt .\code.txt")
-        shutil.move("./temporary/code.txt", "./code.txt")
+        shutil.move("./temporary/code.txt", "./run/code.txt")
+
         print(hint_wrapper("Start the simulation ..."))
-        os.system("./mips.exe -nolog -tclbatch ./run/mips.tcl > ./temporary/cpu.txt")
+        os.chdir(current_path / "run")
+        os.system("./mips.exe -nolog -tclbatch ./mips.tcl > ../temporary/cpu.txt")
+        os.chdir(current_path)
 
         print(hint_wrapper("Run Mars to get the right output ..."))
         if no_db:
@@ -59,6 +70,5 @@ def run():
 
 
 if __name__ == "__main__":
-    current_path = pathlib.Path(__file__).resolve().parent
     os.chdir(current_path)
     run()
